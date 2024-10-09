18842e49d47466707106a502297c279721a9fabb
kebab <darknoobssquad@gmail.com>
2024-10-09 23:46:19 +0300

    webpack+plugins

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, // cleans the dist folder before each build
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]',
+                    },
+                },
+            },
+        ],
+    },
+    devtool: 'inline-source-map', // for debugging
+    devServer: {
+        static: './dist', // serve files from the dist folder
+    },
+};
